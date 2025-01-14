38d034adb33c753b4563bed749b397f11f304860
Amine BIRANI <a.biranielhbak@gmail.com>
2025-01-14 19:27:17 +0000

    update(app): Use poke-list in main app component

--- apps/nx-pokedex/src/app/app.component.ts
+++ apps/nx-pokedex/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, PokeListComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',

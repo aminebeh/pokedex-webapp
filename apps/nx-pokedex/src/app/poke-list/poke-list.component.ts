db46c06206bebdd2617c150f127199fe7bae3e96
Amine BIRANI <a.biranielhbak@gmail.com>
2025-01-14 19:26:17 +0000

    chore(poke-list): Generate poke-list component

---
+++ apps/nx-pokedex/src/app/poke-list/poke-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-poke-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './poke-list.component.html',
+  styleUrl: './poke-list.component.css',
+})
+export class PokeListComponent {}

29bb1977b84ce22c4a3c11bc724d4243aab1d3cd
Amine BIRANI <a.biranielhbak@gmail.com>
2025-01-17 17:55:28 +0000

    feat(pokedex): Implement service
    - Get mock pokemons
    - Affect random status for testing purposes

--- apps/nx-pokedex/src/app/services/pokedex.service.ts
+++ apps/nx-pokedex/src/app/services/pokedex.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { pokemons } from './pokemon-list.mock';
+
+export interface IPokemon {
+  name: string;
+  id: number;
+  sprites: {
+    icons: { front_default: string };
+    showdown: string;
+  };
+}
+
+export enum PokedexEntryStatus {
+  Caught = 'caught',
+  Seen = 'seen',
+  None = 'none',
+}
+
+export interface IPokedexEntry extends IPokemon {
+  status: PokedexEntryStatus;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokedexService {
+  getPokemons(): Observable<IPokemon[]> {
+    return of(pokemons);
+  }
 
-  constructor() { }
+  getPokedexEntries(): Observable<IPokedexEntry[]> {
+    return of(
+      pokemons.map((pokemon) => ({
+        ...pokemon,
+        status: this.getRandomStatus(),
+      }))
+    );
+  }
+
+  private getRandomStatus(): PokedexEntryStatus {
+    const statuses = Object.values(PokedexEntryStatus);
+    const randomIndex = Math.floor(Math.random() * statuses.length);
+    return statuses[randomIndex];
+  }
 }

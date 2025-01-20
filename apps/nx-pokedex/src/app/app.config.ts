7ab21a2745df0e409c96a38d13632992b613402b
Amine BIRANI <a.biranielhbak@gmail.com>
2025-01-20 15:58:37 +0000

    feat(nx-pokedex): Add animations to the project

--- apps/nx-pokedex/src/app/app.config.ts
+++ apps/nx-pokedex/src/app/app.config.ts
@@ -1,4 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
@@ -6,5 +8,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    /* Uncomment if immediate animation is needed in the application (@see: https://v18.angular.dev/guide/animations) */
+    //provideAnimations
+    provideAnimationsAsync(),
   ],
 };

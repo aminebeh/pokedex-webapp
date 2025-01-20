3579f55aaee477e3a74e060e62c19cfe4baaf0d9
Amine BIRANI <a.biranielhbak@gmail.com>
2025-01-20 15:21:35 +0000

    deps(angular-animation): Add angular-animation to project

--- apps/nx-pokedex/src/app/app.component.ts
+++ apps/nx-pokedex/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, PokeListComponent],
+  imports: [RouterModule, PokeListComponent, BrowserAnimationsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',

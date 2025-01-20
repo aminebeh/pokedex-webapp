16f1244b68be80d54fc4ea71d1374024f3990eb6
Amine BIRANI <a.biranielhbak@gmail.com>
2025-01-20 15:18:52 +0000

    refacto(nx-pokedex): Clean app component from router-outlet and nxwelcome

--- apps/nx-pokedex/src/app/app.component.ts
+++ apps/nx-pokedex/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, PokeListComponent],
+  imports: [RouterModule, PokeListComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
